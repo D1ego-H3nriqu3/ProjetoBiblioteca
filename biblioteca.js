797dcfd504a768c9c418a26f347d409b82428371
D1ego-H3nriqu3 <dh6451013@gmail.com>
2026-09-23 11:29:47 -0300

    feat: adiciona menu principal e sua verificação lógica

--- biblioteca.js
+++ biblioteca.js
@@ -1,2 +1,37 @@
 import PromptSync from "prompt-sync";
 const prompt = PromptSync()
+
+let livro = []
+
+let indice = true
+
+do{
+    console.log(`\n=== Biblioteca Pessoal ===
+1. Cadastrar livro
+2. Listar livros
+3. Atualizar status de leitura
+4. Remover livro
+0. Sair`)
+    let resposta = (prompt("Escolha uma opção: ")).trim()
+
+    switch(resposta){
+        case("1"):
+
+            break
+        case("2"):
+
+            break
+        case("3"):
+
+            break
+        case("4"):
+
+            break
+        case("0"):
+
+            break
+        default:
+            console.log("\nEscolha uma opção correspondente;")
+    }
+
+}while(indice === true)
